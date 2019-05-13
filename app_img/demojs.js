b057e0252bd3ef459eae0211f55efd29e31e42cc
Baibing <bl285@uowmail.edu.au>
2019-05-13 18:13:47 +1000

    Initial commit

---
+++ app_img/demojs.js
@@ -0,0 +1,20 @@
+// $("#img01").click(function(){
+//     $(this).toggleClass('min');
+//     $(this).toggleClass('max');
+// });
+//
+//
+// function image(img) {
+//     var src = img.src;
+//     window.open(src);
+// }
+
+
+
+
+// Get the modal
+
+
+
+
+
